fc96637e3bda13f3ca0b69d64f534f5064f93734
nawta <wes904@icloud.com>
2025-01-09 16:45:17 +0900

    added x11vnc, with password

---
+++ docker/start_vnc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set VNC password
+mkdir -p ~/.vnc
+x11vnc -storepasswd "yourpassword" ~/.vnc/passwd
+
+# Start Xvfb (virtual framebuffer) on display :0
+Xvfb :0 -screen 0 1280x1024x24 &
+
+# Export DISPLAY variable
+export DISPLAY=:0
+
+# Start fluxbox (lightweight window manager)
+fluxbox &
+
+# Start x11vnc server
+x11vnc -display :0 -forever -passwd "yourpassword"
